72c0cc7b308920dbe6affd6fbcaf5caf48231c3a
JulenZZZ <julenz485@gmail.com>
2023-11-30 22:27:06 +0100

    adding [(ngModule)] and [ngStyle] to Empleado component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FrutaComponentComponent } from './fruta-component/fruta-component.component';
 import { EmpleadosComponent } from './empleados/empleados.component';
+import { FormsModule, ReactiveFormsModule  } from "@angular/forms";
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FrutaComponentComponent, EmpleadosComponent],
+  imports: [CommonModule, RouterOutlet, FrutaComponentComponent, EmpleadosComponent,FormsModule,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/empleados/empleados.component.ts
+++ src/app/empleados/empleados.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Empleado } from './empleado';
+import { FormsModule  } from "@angular/forms";
 
 @Component({
   selector: 'empleado-tag',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './empleados.component.html',
   styleUrl: './empleados.component.css'
 })
@@ -13,7 +14,10 @@ export class EmpleadosComponent {
   public titulos='Componente Empleados';
   public empleado!: Empleado;
   public trabajadores:Array<Empleado>;
+  public personas:Array<any>;
   public trabajador_externo:boolean;
+  public color:string;
+  public color_seleccionado:any;
 
   constructor(){
 
@@ -25,18 +29,31 @@ export class EmpleadosComponent {
       new Empleado('Julio Grejner',66,'Cocinero',false)
     ];
     this.trabajador_externo = false;
+    this.color = 'blue';
+    /*using [(ngModule)] and useValue() function to use two way data-binding on view*/
+    this.color_seleccionado = '#ccc'
 
+    this.personas = ['Benancio',22,'Arturo',11,'Carlos',55];
   }
 
   ngOnInit(){
 
     console.log(this.empleado);
     console.log(this.trabajadores);
+    
   }
 
   cambiarExterno(valor:any){
 
     this.trabajador_externo = valor;
+  }
+
+  setValue(){
+    this.color_seleccionado='red';
+  }
+
+  logColorSeleccionado(){
+    console.log(this.color_seleccionado);
   }
   
 }
